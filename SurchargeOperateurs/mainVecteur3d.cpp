4e911272e0beba4d2be749b7d258a29cc903b91e
Astaxanthine <geoffreybourreau@outlook.fr>
2021-08-17 07:28:12 +0200

    Modification du main

--- SurchargeOperateurs/mainVecteur3d.cpp
+++ SurchargeOperateurs/mainVecteur3d.cpp
@@ -1,23 +1,9 @@
 #include "vecteur3d.h"
 #include <iostream>
 using namespace std;
-/*
-class vecteur3d{
-	float v[3];
-  public:
-	vecteur3d(float c1=0.0, float c2=0.0, float c3=0.0){
-		v[0] = c1;
-		v[1] = c2;
-		v[2] = c3;
-	}
-	float & operator [](int);
-};
 
-float & vecteur3d::operator [](int i){
-	if((i<0) || (i>2)) i = 0; //Debordement d'indice
-	return v[i];
-}
-*/
+//Exercice sur la surcharge de l'opérateur []
+
 int main(){
 	vecteur3d v(3,4,5);
 	int i;
